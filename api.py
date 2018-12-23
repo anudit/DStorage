4333830f739addc0edc79764405265c0098e5108
Anudit Nagar <nagaranudit@gmail.com>
2018-12-23 18:49:01 +0530

    Initialize Codebase

---
+++ api.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template
+from werkzeug import secure_filename
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = './uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+
+UPLOAD_FOLDER = os.path.basename('uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html", title = 'Index')
+
+@app.route('/upload')
+def upload():
+   return render_template("upload.html", title = 'Index')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+      return 'file uploaded successfully'
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='127.0.0.1', port=port)
